2239ca4de96b06f26945fd0f871f2e4fa14b1d6c
John Wolfe <johntmwolfe@gmail.com>
2020-12-02 21:43:22 -0500

    Search functionality complete (may be buggy)

--- user_query.py
+++ user_query.py
@@ -40,7 +40,7 @@ def create_connection(db_file):
 
 
 
-def process(parser, line):
+def process(parser):
 	liz = []
 	while len(parser) > 0:
 		letter = parser[:1]
@@ -67,6 +67,8 @@ def process(parser, line):
 			liz.append("Other_Sales")
 		elif letter == "b":
 			liz.append("Global_Sales")
+		elif letter == "*":
+			liz.append("*")
 		else:
 			print("Not a valid string.")
 			err = ["ERR"]
@@ -76,7 +78,7 @@ def process(parser, line):
 
 
 def att_grab():
-	print("Which attributes do you want to apply?\n1. Standing (row in the DB)\n2. Name\n3. Platform\n4. Year\n5. Genre\n6. Publisher\n7. North American Sales\n8. European Sales\n9. Japan Sales\na. Other sales\nb.Global sales\nType which you want: ")
+	print("Which attribute do you want to use?\n1. Standing (row in the DB)\n2. Name\n3. Platform\n4. Year\n5. Genre\n6. Publisher\n7. North American Sales\n8. European Sales\n9. Japan Sales\na. Other sales\nb.Global sales\nType which you want: ")
 	val = raw_input()
 	if val == "1":
 		return "standing"
@@ -102,16 +104,21 @@ def att_grab():
 		return "Global_Sales"
 	else:
 		print("Not an attribute. Pick better next time.")
-		return "ERR"
+		return att_grab()
 
 
-def conditions(select):
-	looped = false
+
+
+
+
+
+def conditions(select, single):
+	looped = False
 	clone = select + " where ";
 	menu(3)
 	val = raw_input()
-	while val != "5":
-		looped = true
+	while val != "6":
+		looped = True
 		if val == "1":
 			val = att_grab()
 			while val=="ERR":
@@ -122,7 +129,6 @@ def conditions(select):
 			first = val[:split]
 			last = val[split+1:]
 			clone += first + " and " + last
-			print(clone)
 		elif val == "2":
 			val = att_grab()
 			while val=="ERR":
@@ -138,9 +144,8 @@ def conditions(select):
 			liz.append(val)
 			clone += "("
 			for item in liz:
-				clone += item + ", "
+				clone +=  "\"" + item.strip() + "\", "
 			clone = clone[:-2] + ")"
-			print(clone)
 		elif val == "3":
 			val = att_grab()
 			while val=="ERR":
@@ -148,20 +153,54 @@ def conditions(select):
 			clone += val + " like "
 			val = raw_input("Enter the matcher value (it is case sensitive, so be specific): ")
 			clone += "\'%" + val + "%\'"
-			print(clone)
 		elif val == "4":
+			menu(5)
+			val = raw_input()
+			liz = process(val)
+			while liz[0]=="ERR":
+				menu(5)
+				val = raw_input()
+				liz = process(val)
+			clone += "("
+			for item in liz:
+				clone += item + " | "
+			clone = clone[:-3] + ")"
+			val = raw_input("Are you wanting to check if this is NULL, or filled?\n1. NULL\n2. Not NULL\n")
+			while val != "1" and val != "2":
+				val = raw_input("Are you wanting to check if this is NULL, or filled?\n1. NULL\n2. Not NULL\n")
+			if val == "1":
+				clone += " is NULL"
+			else:
+				clone += " is not NULL"
+		elif val == "5":
 			val = att_grab()
-			while val=="ERR":
-				val = att_grab()
-		#elif val == "5":
-		#else:
-		#	print("Invalid response. Try again.")
-		#	return "ERR"
+			clone += " " + val
+			menu(6)
+			val = raw_input()
+			while int(val) > 6 or int(val) < 1:
+				menu(6)
+				val = raw_input()
+			comparator = raw_input("Comparator value: ")
+			if val == "1":
+				clone += " > " + comparator
+			elif val == "2":
+				clone += " >= " + comparator
+			elif val == "3":
+				clone += " < " + comparator
+			elif val == "4":
+				clone += " <= " + comparator
+			elif val == "5":
+				clone += " = " + comparator
+			else:
+				clone += " != " + comparator
 		time.sleep(.2)
-		menu(4)
-		val = raw_input()
-		if val != 5:	
-			clone += " and "
+		if (single):
+			val = "6";
+		else:
+			menu(4)
+			val = raw_input()
+			if val != "6":	
+				clone += " and "
 	if looped:
 		return clone
 	else:
@@ -169,27 +208,67 @@ def conditions(select):
 
 
 
+def order(line):
+	#print(up_down[:1])
+	val = ""
+	while val != "no" and val != "n":
+		val = att_grab()
+		up_down = raw_input("Order by ascending or descending? (asc/desc): ")
+		if up_down == "a" or up_down == "asc":
+			line += " " + val + " asc,"
+		elif up_down == "d" or up_down == "desc":
+			line += " " + val + " desc,"
+		else:
+			print("Try again, idiot.")
+		val = raw_input("Want to sort further? (yes/no): ")
+	return line[:-1]
+
+
 
 def search(conn):
 	str = "select "
 	menu(2)
 	val = raw_input()
-	liz = process(val,str)
+	liz = process(val)
 	if liz[0] != "ERR":
-		for attrib in liz:
-			str += attrib + ", "
-		str = str[:-2] + " "
+		if liz[0] == "*":
+			print
+			str += "* "
+		else:
+			for attrib in liz:
+				str += attrib + ", "
+			str = str[:-2] + " "
 		print(str)
 		str += "from sales"
-		str = conditions(str)
-	#cur = conn.cursor()
-	#cur.execute(str)
+		str = conditions(str, False)
+		print(str)
+		val = raw_input("Do you want the group the output to a certain attribute? \n(yes/no): ")
+		if val == "yes" or val == "y":
+			print("Which attribute?\n")
+			val = att_grab()
+			str += "group by " + val + " "
+			val = raw_input("Do you want to have a condition for these groupings?\n(yes/no): ")
+			if val == "yes" or val == "y":
+				str = conditions(str,True)
+				print(str)
+		val = raw_input("Do you want the output sorted?\n(y/n): ")
+		if val == "yes" or val == "y":
+			str += " order by"
+			str = order(str)
+			print(str)
+		val = raw_input("Do you want a limit to the return values? (y/n): ")
+		if val == "yes" or val == "y":
+			val = raw_input("Limit by how many?: ")
+			str += " limit " + val
+			print(str)
+		cur = conn.cursor()
+		print(str)
+		cur.execute(str)
 
-	#rows = cur.fetchall()
+		rows = cur.fetchall()
 
-	#for row in rows:
-	#	print(row)
-"""how to select by a certain priority == ("select * from tasks where priority=?", (priority,))"""
+		for row in rows:
+			print(row)
 
 
 
@@ -289,9 +368,12 @@ def menu(int):
 	switcher = {
 		0: "\n\nWelcome to the video game sales database!\nWhat would you like?\nSearch for games (search/s)\nWatch a particular game (watch/w)\nLook through data about games (data/d)\nQuit application (quit/q)\n",
 		1: "\nMultiple games match that search. Can you be more specific?\nChoose a more specific name(n)\nChoose a year(y)\nChoose a genre(g)\nChoose a console of platform(c)\nPrint the games you have so far(p)\n",
-		2: "\nWhich attributes do you want to see?\n1. Standing (row in the DB)\n2. Name\n3. Platform\n4. Year\n5. Genre\n6. Publisher\n7. North American Sales\n8. European Sales\n9. Japan Sales\na. Other sales\nb.Global sales\nType the letters and numbers you want, in the order you want them: ",
-		3: "\nDo you want a certain condition on the data?\n\n1. An attribute value within a certain range\n2. Check if value matches a list of values (genre == \"Platform, Action, or Misc\")\n3. Answer includes a portion of the answer (Name includes \"Bear\")\n4. Include if this column's empty\n5. None of the above\n",
-		4: "\nAny other conditions?\n\n1. An attribute value within a certain range\n2. Check if value matches a list of values (genre == \"Platform, Action, or Misc\")\n3. Answer includes a portion of the answer (Name includes \"Bear\")\n4. Include if this column's empty\n5. None of the above\n"}
+		2: "\nWhich attributes do you want to see?\n1. Standing (row in the DB)\n2. Name\n3. Platform\n4. Year\n5. Genre\n6. Publisher\n7. North American Sales\n8. European Sales\n9. Japan Sales\na. Other sales\nb.Global sales\nType the letters and numbers you want, in the order you want them.\nSearch(if you want them all, put a *): ",
+		3: "\nDo you want a certain condition on the data?\n\n1. An attribute value within a certain range\n2. Check if value matches a list of values (genre == \"Platform, Action, or Misc\")\n3. Answer includes a portion of the answer (Name includes \"Bear\")\n4. Include if this column's empty\n5. A mathematical comparison(less than, greater than, equal to)\n6. None of the above\n",
+		4: "\nAny other conditions?\n\n1. An attribute value within a certain range\n2. Check if value matches a list of values (genre == \"Platform, Action, or Misc\")\n3. Answer includes a portion of the answer (Name includes \"Bear\")\n4. Include if this column's empty\n5. A mathematical comparison(less than, greater than, equal to)\n6. None of the above\n",
+		5: "\nWhich attributes do you want to check?\n1. Standing (row in the DB)\n2. Name\n3. Platform\n4. Year\n5. Genre\n6. Publisher\n7. North American Sales\n8. European Sales\n9. Japan Sales\na. Other sales\nb.Global sales\nType the letters and numbers you want, in the order you want them: ",
+		6: "\nWhich conditional?\n1. >\n2. >=\n3. <\n4. <=\n5. =\n6. !=\n"
+		}
 	print switcher.get(int, "ERROR")
 
 
